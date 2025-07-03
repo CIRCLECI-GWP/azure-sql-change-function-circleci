f3a8aad5e4f1b72b78c609071fd0f935f6c853e1
Agusioma <tcreatesllc@gmail.com>
2025-07-03 21:54:35 +0300

    init

--- poll_changes/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-import azure.functions as func
-
-def main(changes: func.SqlRowList) -> None:
-    logging.warning("⚡ Function was triggered successfully")
---
+++ poll_changes/function_app.py
@@ -0,0 +1,12 @@
+import json
+import logging
+import azure.functions as func
+
+app = func.FunctionApp()
+
+@app.function_name(name="mytriggerfn")
+@app.sql_trigger(arg_name="todo",
+                        table_name="Sales",
+                        connection_string_setting="SQL_CONNECTION_STRING")
+def todo_trigger(todo: str) -> None:
+    logging.info("SQL Changes: %s", json.loads(todo))
