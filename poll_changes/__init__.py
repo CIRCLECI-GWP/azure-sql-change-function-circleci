346b51885460a72372cf143571898c12875a6642
Agusioma <tcreatesllc@gmail.com>
2025-07-03 11:32:47 +0300

    init

--- poll_changes/__init__.py
+++ poll_changes/__init__.py
@@ -1,9 +1,16 @@
 import logging
+import traceback
 import azure.functions as func
 
 def main(changes: func.SqlRowList) -> None:
-    logging.info(f"[Sales] {len(changes)} change(s) detected.")
-    for row in changes:
-        sale_id = row.get("Id")
-        sale_date = row.get("SaleDate")
-        logging.info(f"SaleID={sale_id}, SaleDate={sale_date}")
+    try:
+        logging.info(f"[Sales] {len(changes)} change(s) detected.")
+        for row in changes:
+            sale_id = row.get("Id")
+            sale_date = row.get("SaleDate")
+            logging.info(f"SaleID={sale_id}, SaleDate={sale_date}")
+
+    except Exception as e:
+        logging.error("An error occurred while processing SQL changes.")
+        logging.error(f"Exception: {str(e)}")
+        logging.error("Stack Trace:\n" + traceback.format_exc())
