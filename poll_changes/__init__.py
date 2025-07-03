1311c0f2693f55db78525dbe59996cd4f9129761
Agusioma <tcreatesllc@gmail.com>
2025-07-03 21:14:03 +0300

    init

--- poll_changes/__init__.py
+++ poll_changes/__init__.py
@@ -1,11 +1,5 @@
 import logging
-import json
 import azure.functions as func
 
 def main(changes: func.SqlRowList) -> None:
-    try:
-        logging.info(f"✅ SQL Trigger fired: {len(changes)} row(s) detected.")
-        for row in changes:
-            logging.info(f"Change row: {row}")
-    except Exception as e:
-        logging.error(f"❌ Error processing SQL changes: {str(e)}")
+    logging.warning("⚡ Function was triggered successfully")
